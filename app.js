ee60d57170f77c366a690827af595ccf678cdd90
Kirill Belov <showc7@gmail.com>
2015-08-07 22:33:10 +0300

    modified:   app.js:	express-route-tree example

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-var express = require('express')
-var app = express()
+// File: app.js
 
-console.log('started');
+var express = require('express'),
+    app = express(),
+    route = require('express-route-tree');
 
-app.get('/', function(req,res){
-	res.send('hello world');
-})
+app.use(route(__dirname + '/controller'));
+/*
+// Advanced Settings
+var fileRouter = ['robots.txt'],
+    shortAddress = ['mon', 'tus'],
+    regionRoute = { china: { id: '1', name: '中国' } };
+app.use(route(__dirname + '/controller', function(req, res, next, controller) {
+    var pathname = req.path.substring(1);
+    if (~fileRouter.indexOf(pathname)) {
+        return res.sendFile(pathname, { maxAge: 3600 * 24 * 1000 });
+    } else if (~shortAddress.indexOf(pathname))) {
+        // some short address
+        return controller.index.season(req, res, next, shortAddress[pathname] + 1);
+    } else if (regionRoute[pathname]) {
+        return controller.index.region(req, res, next, regionRoute[pathname].id, regionRoute[pathname].name);
+    } else {
+        return next('No such route.');
+    }
+});
+*/
+
+// try it:
+// console.log(route.controller);
 
 app.listen(3000);
